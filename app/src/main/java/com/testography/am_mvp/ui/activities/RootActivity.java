691ea385f5a63e9571851005ba984481008ca3dc
Leo Burdianov <leo.burdianov@gmail.com>
2016-10-22 15:59:38 +0400

    Implement the basic structure

--- app/src/main/java/com/testography/am_mvp/ui/activities/RootActivity.java
+++ app/src/main/java/com/testography/am_mvp/ui/activities/RootActivity.java
@@ -1,15 +1,121 @@
 package com.testography.am_mvp.ui.activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
+import com.testography.am_mvp.BuildConfig;
 import com.testography.am_mvp.R;
+import com.testography.am_mvp.mvp.presenters.AuthPresenter;
+import com.testography.am_mvp.mvp.presenters.IAuthPresenter;
+import com.testography.am_mvp.mvp.views.IAuthView;
+import com.testography.am_mvp.ui.custom_views.AuthPanel;
 
-public class RootActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class RootActivity extends AppCompatActivity implements IAuthView, View.OnClickListener {
+
+    AuthPresenter mPresenter = AuthPresenter.getInstance();
+
+    @BindView(R.id.coordinator_container)
+    CoordinatorLayout mCoordinatorLayout;
+
+    @BindView(R.id.auth_wrapper)
+    AuthPanel mAuthPanel;
+
+    @BindView(R.id.show_catalog_btn)
+    Button mShowCatalogBtn;
+
+    @BindView(R.id.login_btn)
+    Button mLoginBtn;
+
+    //region ========== Life cycle ==========
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_root);
+        ButterKnife.bind(this);
+
+        mPresenter.takeView(this);
+        mPresenter.initView();
+
+        mLoginBtn.setOnClickListener(this);
+        mShowCatalogBtn.setOnClickListener(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        mPresenter.dropView();
+        super.onDestroy();
+    }
+
+    //endregion
+
+    @Override
+    public void showMessage(String message) {
+        Snackbar.make(mCoordinatorLayout, message, Snackbar.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void showError(Throwable e) {
+        if (BuildConfig.DEBUG) {
+            showMessage(e.getMessage());
+            e.printStackTrace();
+        } else {
+            showMessage("Something went wrong, try again later");
+            // TODO: 22-Oct-16 send error stacktrace to crashlytics
+        }
+    }
+
+    @Override
+    public void showLoad() {
+        // TODO: 22-Oct-16 show load progress 
+    }
+
+    @Override
+    public void hideLoad() {
+        // TODO: 22-Oct-16 hide load progress
+    }
+
+    @Override
+    public IAuthPresenter getPresenter() {
+        return mPresenter;
+    }
+
+    @Override
+    public void showLoginBtn() {
+        mLoginBtn.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void hideLoginBtn() {
+        mLoginBtn.setVisibility(View.GONE);
+    }
+
+//    @Override
+//    public void testShowLoginCard() {
+//        mAuthCard.setVisibility(View.VISIBLE);
+//    }
+
+    @Override
+    public AuthPanel getAuthPanel() {
+        return mAuthPanel;
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.show_catalog_btn:
+                mPresenter.clickOnShowCatalog();
+                break;
+            case R.id.login_btn:
+                mPresenter.clickOnLogin();
+                break;
+        }
     }
 }
