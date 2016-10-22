aaf756dd651b5953e8282c712b5ceb978bb2c9ac
Leo Burdianov <leo.burdianov@gmail.com>
2016-10-22 12:15:39 +0400

    Initial commit including the gradle dependencies

---
+++ app/src/main/java/com/testography/am_mvp/ui/activities/RootActivity.java
@@ -0,0 +1,15 @@
+package com.testography.am_mvp.ui.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.testography.am_mvp.R;
+
+public class RootActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_root);
+    }
+}
